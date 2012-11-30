ea79c2b51f6596b5f44cf1a7a4a9631ad9187d8f
Ashley Whetter <awhetter.2011@my.bristol.ac.uk>
2012-11-30 11:09:48 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CogsScreen::Application.config.secret_token = '1e8cd8189a77478646fde0b12923d50efbcecffd8776ef7685ce305bfa59ea40836f18722f2312854bee474d8c88ec220f37fa570ed03efd1c318758648765e0'
