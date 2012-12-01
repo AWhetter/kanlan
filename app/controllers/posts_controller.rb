3bdeaa3e63363ebfbbf245d92c4af9cd9b1d42f0
Ashley Whetter <awhetter.2011@my.bristol.ac.uk>
2012-12-01 10:11:28 +0000

    Fixed users being able to post the same game multiple times

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,20 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(:user => User.find(session[:user_id]))
+    user = User.find(session[:user_id])
+    game = Game.find(params[:post][:game])
+    @post = Post.new(:user => user)
     @post.assign_game params[:post][:game]
+
+    if user.posts.size > 0
+      user.posts.each do |post|
+        if post.games == [game]
+          flash[:notice] = "You have already posted that game!"
+          render action: "new"
+          return
+        end
+      end
+    end
 
     if @post.save
       redirect_to root_url, :notice => "Successfully created post!"
