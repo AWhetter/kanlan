14a109ea9e1cc1d577200623b6b8fc7890a81ee1
Ashley Whetter <awhetter.2011@my.bristol.ac.uk>
2012-12-02 13:33:10 +0000

    Added some games in the seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Game.create( :name => "Counter Strike: Source", :website => "http://store.steampowered.com/app/240/" )
+Game.create( :name => "Planetside 2", :website => "http://store.steampowered.com/app/218230/" )
+Game.create( :name => "Left 4 Dead 2", :website => "http://store.steampowered.com/app/550/" )
+Game.create( :name => "Borderlands 2", :website => "http://store.steampowered.com/app/49520/" )
+Game.create( :name => "Team Fortress 2", :website => "http://store.steampowered.com/app/440/" )
