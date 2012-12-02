06432fb826fe2307bcef77656a13a52157e0ceed
Ashley Whetter <awhetter.2011@my.bristol.ac.uk>
2012-12-02 14:23:18 +0000

    Fixed seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Game.create( :name => "Counter Strike: Source", :website => "http://store.steampowered.com/app/240/" )
-Game.create( :name => "Planetside 2", :website => "http://store.steampowered.com/app/218230/" )
-Game.create( :name => "Left 4 Dead 2", :website => "http://store.steampowered.com/app/550/" )
-Game.create( :name => "Borderlands 2", :website => "http://store.steampowered.com/app/49520/" )
-Game.create( :name => "Team Fortress 2", :website => "http://store.steampowered.com/app/440/" )
+Game.create( :name => "Counter Strike: Source", :url => "http://store.steampowered.com/app/240/" )
+Game.create( :name => "Planetside 2", :url => "http://store.steampowered.com/app/218230/" )
+Game.create( :name => "Left 4 Dead 2", :url => "http://store.steampowered.com/app/550/" )
+Game.create( :name => "Borderlands 2", :url => "http://store.steampowered.com/app/49520/" )
+Game.create( :name => "Team Fortress 2", :url => "http://store.steampowered.com/app/440/" )
