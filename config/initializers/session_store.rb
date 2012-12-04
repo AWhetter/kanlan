0d130e41f66de2ad9521ee652bdbe29b7db6a3ad
Ashley Whetter <awhetter.2011@my.bristol.ac.uk>
2012-12-04 16:51:48 +0000

    Made sessions expires after 24 hours

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CogsScreen::Application.config.session_store :cookie_store, key: '_cogsScreen_session'
+CogsScreen::Application.config.session_store :cookie_store, key: '_cogsScreen_session', :expire_after => 24.hours
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
