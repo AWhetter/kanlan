229ccc0b026c6eb8041a95a6c045002c0ec7a2c6
Ashley Whetter <awhetter.2011@my.bristol.ac.uk>
2013-01-26 23:49:55 +0000

    Added more useful error message when a user tries to sign in with a non-existent username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,9 @@ class SessionsController < ApplicationController
     if user
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Hello again, " + params[:username] + "!"
+    elsif User.find_by_username(params[:username]).nil?
+      flash.now[:notice] = "No user exists with that username. Please sign up."
+      render "new"
     else
       flash.now[:notice] = "Invalid username for this IP address!"
       render "new"
