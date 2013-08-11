5f1aa9c6127ae1182ee6039034e829964058b222
Ashley Whetter <ashley@awhetter.co.uk>
2013-08-11 11:11:52 +0100

    Added games/create endpoint
    Implemented basic functionality.
    Implemented tests.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,5 +3,18 @@ class GamesController < ApplicationController
   end
 
   def create
+    game = Game.new(game_params)
+    if game.save
+      flash[:notice] = "Created #{game.name}!"
+      redirect_to new_post_path
+    else
+      flash[:error] = "Game not created!"
+      redirect_to new_game_path
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:name, :url)
   end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -10,13 +10,56 @@ describe GamesController do
   end
 
   describe "create endpoint" do
+    let(:new_game) { FactoryGirl.attributes_for(:game) }
     context "given an invalid game" do
-      it "does not create a game"
+      let!(:old_count) do
+        game_obj = Game.new(new_game)
+        expect(game_obj.save).to be true
+        expect(game_obj).to_not be_new_record
+
+        Game.count
+      end
+
+      before do
+        post :create, :game => new_game
+      end
+
+      it "does not create the game" do
+        game_obj = Game.new(new_game)
+        expect(game_obj.save).to be false
+        expect(Game.count).to eq(old_count)
+      end
+
+      it "redirects to creating a new game" do
+        expect(response).to redirect_to(new_game_path)
+      end
+
+      it "returns an error" do
+        expect(flash[:error]).to_not be nil
+      end
+
+      it "gives a useful error message depending on the given parameters"
     end
 
     context "given a valid game" do
-      it "creates a game"
+      let!(:old_count) { Game.count }
+      before do
+        post :create, :game => new_game
+      end
+
+      it "creates the game" do
+        game_obj = Game.new(new_game)
+        expect(game_obj.save).to be false
+        expect(Game.count).to eq(old_count + 1)
+      end
+
+      it "redirects to creating a new post" do
+        expect(response).to redirect_to(new_post_path)
+      end
+
+      it "returns a status message" do
+        expect(flash[:notice]).to_not be nil
+      end
     end
   end
-
 end
